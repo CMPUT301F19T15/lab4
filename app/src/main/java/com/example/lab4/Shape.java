1671482856dfb6c679633fb542e370f44428139e
Jorden Hansen <jorden1@ualberta.ca>
2019-09-30 17:03:34 -0600

    fixed class
    made abstract

--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -1,27 +1,7 @@
 package com.example.lab4;
 
-public class Shape {
+public abstract class Shape {
     int x;
     int y;
 
-    public Shape(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
 }
